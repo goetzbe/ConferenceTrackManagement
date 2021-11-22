24d08ee5737860b3a1a40f24e19fc5d18a135644
Benedikt Götz <goetzb@hochschule-trier.de>
2021-11-22 10:23:12 +0100

    Added Unit Tests for Time Format Output

--- UnitTests/UnitTests.cpp
+++ UnitTests/UnitTests.cpp
@@ -1,6 +1,10 @@
+#include <iostream>
 #include "pch.h"
+
 #include "CppUnitTest.h"
 #include "Talk.h"
+#include "AdditionalTypes.h"
+
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -9,20 +13,48 @@ namespace UnitTests
 	TEST_CLASS(InitializingTests)
 	{
 	public:
-		
 		TEST_METHOD(InitalizeTalkWithMinutes)
 		{
 			std::string inputString = "Writing Fast Tests Against Enterprise Rails 60min";
 			Talk task = Talk{inputString};
 			Assert::AreEqual(std::string("Writing Fast Tests Against Enterprise Rails"), task.GetName());
-			Assert::AreEqual(60, task.GetLength());
+			Assert::AreEqual(60, task.GetDuration());
 		}
 		TEST_METHOD(InitalizeLightningTalk)
 		{
 			std::string inputString = "Rails for Python Developers lightning";
 			Talk task = Talk{inputString};
 			Assert::AreEqual(std::string("Rails for Python Developers"), task.GetName());
-			Assert::AreEqual(5, task.GetLength());
+			Assert::AreEqual(5, task.GetDuration());
+		}
+	};
+
+	TEST_CLASS(OutputTests)
+	{
+	public:
+		TEST_METHOD(OutputTimeOfDayNoLeadingZerosAM)
+		{
+			std::string expected = "10:15AM";
+
+			std::stringstream buffer;
+
+			TimeOfDay time = TimeOfDay{ 10, 15, Period::AM };
+			buffer << time;
+
+			// Test
+			Assert::AreEqual(expected, buffer.str());
+		}
+		TEST_METHOD(OutputTimeOfDayWithLeadingZerosPM)
+		{
+			std::string expected = "03:05PM";
+
+			std::stringstream buffer;
+
+			TimeOfDay time = TimeOfDay{ 3, 5, Period::PM };
+			buffer << time;
+
+			// Test
+			Assert::AreEqual(expected, buffer.str());
 		}
 	};
 }
