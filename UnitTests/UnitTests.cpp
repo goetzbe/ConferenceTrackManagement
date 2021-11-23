03b923d69a8929351edbcda81491fcb6d6d2ca0d
Benedikt Götz <goetzb@hochschule-trier.de>
2021-11-23 11:41:37 +0100

    Added unit test for initializing and printing out a track

--- UnitTests/UnitTests.cpp
+++ UnitTests/UnitTests.cpp
@@ -5,6 +5,7 @@
 #include "AdditionalTypes.h"
 #include "Talk.h"
 #include "Session.h"
+#include "Track.h"
 
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
@@ -109,6 +110,60 @@ namespace UnitTests
 			Assert::AreEqual(expected, buffer.str());
 			Assert::AreEqual(180 - 60 - 45 - 30, session.GetFreeMinutes());
 		}
+
+		TEST_METHOD(InitAndOutputTrack)
+		{
+			Track track = Track();
+
+			Session& session1 = track.GetMorningSession();
+
+			// Adding three talks to the session
+			std::string inputString = "Writing Fast Tests Against Enterprise Rails 60min";
+			Talk talk1 = Talk{ inputString };
+			session1.AddTalk(&talk1);
+
+			inputString = "Overdoing it in Python 45min";
+			Talk talk2 = Talk{ inputString };
+			session1.AddTalk(&talk2);
+
+			inputString = "Lua for the Masses 30min";
+			Talk talk3 = Talk{ inputString };
+			session1.AddTalk(&talk3);
+
+			Session session2 = track.GetAfternoonSession();
+
+			// Adding three talks to the session
+			std::string inputString = "Ruby Errors from Mismatched Gem Versions 45min";
+			Talk talk4 = Talk{ inputString };
+			session2.AddTalk(&talk4);
+
+			inputString = "Common Ruby Errors 45min";
+			Talk talk5 = Talk{ inputString };
+			session2.AddTalk(&talk5);
+
+			inputString = "Rails for Python Developers lightning";
+			Talk talk6 = Talk{ inputString };
+			session2.AddTalk(&talk6);
+
+
+			std::string expected = 
+				"09:00AM Writing Fast Tests Against Enterprise Rails 60min\n"
+				"10:00AM Overdoing it in Python 45min\n"
+				"10:45AM Lua for the Masses 30min\n"
+				"12:00PM Lunch"
+				"01:00PM Ruby Errors from Mismatched Gem Versions 45min"
+				"01:45PM Common Ruby Errors 45min"
+				"02:30PM Rails for Python Developers lightning"
+				"04:00PM Networking Event";
+
+			// Output session
+			std::stringstream buffer;
+			buffer << track;
+
+			// Test
+			Assert::AreEqual(expected, buffer.str());
+		}
+
 	};
 
 	TEST_CLASS(OperationTests)
